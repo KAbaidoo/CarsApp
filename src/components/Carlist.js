23824822309d65a1eae8dfa4f813bd31ba9549df
Kelvin Abaidoo <kabaidoo@hotmail.com>
2025-10-06 05:20:36 +0000

    Add Carlist

---
+++ src/components/Carlist.js
@@ -0,0 +1,29 @@
+import React, {useState, useEffect} from 'react';
+import {DataGrid} from '@mui/x-data-grid';
+import { SERVER_URL } from '../constants';
+
+function Carlist () {
+    const [cars, setCars] = useState([]);
+    const columns = [
+        { field: 'brand', headerName: 'Brand', width: 200 },
+        { field: 'model', headerName: 'Model', width: 200 },
+        { field: 'color', headerName: 'Color', width: 200 },
+        { field: 'year', headerName: 'Year', width: 200 },
+        { field: 'price', headerName: 'Price', width: 200 },
+    ]
+
+    useEffect(() => {
+        fetch(`${SERVER_URL}api/cars`)
+            .then(response => response.json())
+            .then(data => setCars(data._embedded.cars))
+            .catch(error => console.error('Error fetching car data:', error));
+    }, []);
+
+    return (
+        <div style={{ height: 500, width: '100%' }}>
+            <DataGrid rows={cars} columns={columns} getRowId={(row) => row._links.self.href} />
+        </div>
+        )
+}
+
+export default Carlist;
